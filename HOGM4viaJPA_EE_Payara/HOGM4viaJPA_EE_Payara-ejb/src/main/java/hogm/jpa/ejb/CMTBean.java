ec332433afb0673990a700eca9e98618ebbc6cca
AnghelLeonard <leoprivacy@yahoo.com>
2016-08-07 09:20:46 +0300

    Hibernate OGM 4.0 via Java Persistence API, JEE application under Payara 4
    Hibernate OGM 4.0 via Java Persistence API, JEE application under Payara 4

---
+++ HOGM4viaJPA_EE_Payara/HOGM4viaJPA_EE_Payara-ejb/src/main/java/hogm/jpa/ejb/CMTBean.java
@@ -0,0 +1,28 @@
+package hogm.jpa.ejb;
+
+import hogm.jpa.entities.LuckyNumberEntity;
+import java.util.Random;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+@Named("bean")
+public class CMTBean {
+
+    @PersistenceContext(unitName = "HOGM_JPA_PU")
+    private EntityManager em;
+    
+    private static final Logger LOG = Logger.getLogger(CMTBean.class.getName());        
+
+    public void persistAction() {
+        LuckyNumberEntity luckyNumberEntity = new LuckyNumberEntity();
+        luckyNumberEntity.setLuckynumber(new Random().nextInt(1000000));
+
+        em.persist(luckyNumberEntity);
+        
+        LOG.info("Lucky number was successfully persisted ...");
+    }
+}
